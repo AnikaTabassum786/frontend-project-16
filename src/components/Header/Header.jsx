966f7265685f3dd17473c0b5fb4ca99d15158f14
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-04-14 13:14:47 +0600

    NavLink added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import './Header.css'
+import { NavLink } from 'react-router';
+
 const Header = () => {
     return (
         <div>
-            This is header
+          <p>This is header</p>
+
+          <nav>
+             <NavLink to='/'>Home</NavLink>
+             <NavLink to='/mobiles'>Mobiles</NavLink>
+             <NavLink to='/laptops'>Laptops</NavLink>
+          </nav>
         </div>
     );
 };
